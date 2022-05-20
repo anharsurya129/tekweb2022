f481a269dae976ce97b2d8e81ac3703c4b3df1df
anharsurya129 <97265641+anharsurya129@users.noreply.github.com>
2022-05-20 14:47:38 +0700

    Add files via upload

---
+++ content/artikel.md
@@ -0,0 +1,12 @@
+Cuaca Panas di Indonesia
+Sabtu, 14 Mei 2022
+|
+Oleh Anhar Suryadi, Mahasiswa Universitas Ahmad Dahlan Yogyakarta
+
+Beberapa hari ini, indonesia mengalami cuaca panas yang berbeda di siang hari, pasalnya cuaca panas ini lebih terik dari biasanya. Badan Meteorologi Klimatologi dan Geofisika atau disingkat BMKG mencatat selama periode 1 - 7 Mei 2022, suhu maksimum berkisar antara  33-36,1°C. Suhu maksimum tertinggi berada di daerah Tangerang, Banten dan Kalimarau, Kalimantan Utara.
+Banyak yang mengira jika meningkatnya suhu panas di Indonesia disebabkan karena adanya peningkatan gelombang panas (Heatwave) namun ternyata BMKG menjelaskan bila hal itu bukan menjadi salah satu faktor penyebabnya.
+Menurut BMKG, cuaca panas di Indonesia akhir-akhir ini disebabkan oleh posisi semu Matahari. Saat ini, posisi semu Matahari diketahui ada di wilayah utara ekuator. Hal tersebut menandakan bahwa sebagian daerah di Indonesia mulai masuk musim kemarau.
+Fenomena ini umumnya terjadi di wilayah lintang menengah-tinggi seperti wilayah Eropa dan Amerika. Faktor pemicu fenomena ini yaitu kondisi dinamika atmosfer di lintang menengah. Untuk saat ini sampai pertengahan Mei, BMKG menyebutkan bahwa suhu panas di siang hari masih harus diwaspadai.
+Dampak dari cuaca panas ini adalah
+- Sebagian wilayah indonesia akan mengalami kekeringan. Sementara wilayah lainnya akan mengalami banjir di masa depan
+- Curah hujan yang turun akan berkurang 25% di pulau jawa, Sumatra, bali dan Nusa Tenggara. Sementara disisi lain, curah hujan akan naik 10-20% di Kalimantan, Sulawesi, dan Papua
---
+++ main.js
@@ -0,0 +1,89 @@
+Vue.createApp({
+    data() {
+      return {
+        mesage: "Hello Vue!",
+        header: {
+          nama: "Anhar Suryadi",
+          kelas: "2011016061 | Sistem Informasi | Universitas Ahmad Dahlan",
+          quotes: "Semua dimulai dari mencoba, kita tidak pernah merasakan keberhasilan dan kesuksesan tanpai pernah memulai dan tanpa berani mencoba.",
+          imageprofile: "./img/anhars.jpeg",
+        },
+        social: {
+          ig: {
+            url: "https://www.instagram.com/anh.surya09/",
+            title: "anh.surya09",
+          },
+          fb: {
+            url: "https://web.facebook.com/anhar.ibnrasyid",
+            title: "Anhar Ibn Rasyid",
+          },
+          wa: {
+            url: "https://wa.me/6281231852726/?text=Hi!%20Anhar%0AAku%20ingin%20berteman%20denganmu%20A,%20Apa%20kamu%20mau?",
+            title: "Anhar Surya",
+          },
+          tele: {
+            ulr: "https://t.me/annnhrrrraszhkva",
+            title: "Anharrs"
+          }
+        },
+
+        data_artikel: [],
+        article: null,
+      };
+    },
+    methods: {
+      getArticle() 
+      {
+        axios 
+        .get(
+          "https://raw.githubusercontent.com/anharsurya129/tekweb2022/master/content/articles.json"
+        )
+        .then((res) => {
+          console.log(res.data);
+          this.data_artikel = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      },
+      getMarkdown()
+      {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const article = urlParams.get('article');
+        var converter = new showdown.Converter();
+        console.log(article);
+        axios
+          .get(
+            src="https://raw.githubusercontent.com/anharsurya129/tekweb2022/master/content/" + article
+            )
+          .then((res) => {
+            var html = converter.makeHtml(res.data);
+            this.article = html;
+            console.log(html);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+    beforeMount() {
+      this.getArticle(),
+      this.getMarkdown()
+    },
+}).mount("#arc");
+       
+$('.skill-per').each(function() {
+  var $this = $(this);
+  var per = $this.attr('per');
+  $this.css("width",per+'%');
+  $({animatedValue: 0}).animate({animatedValue: per}, {
+    duration: 1000,
+    step: function(){
+      $this.attr('per', Math.floor(this.animatedValue) + '%');
+    },
+    complete: function(){
+      $this.attr('per', Math.floor(this.animatedValue) + '%');
+    }
+  });
+});
