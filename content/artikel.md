0c8d420c734d647f2515207d663ac5027a31e2d2
anharsurya129 <97265641+anharsurya129@users.noreply.github.com>
2022-05-20 08:19:10 +0700

    Add files via upload

---
+++ content/artikel.md
@@ -0,0 +1,11 @@
+Berjalan Perlahan
+Sabtu, 14 Mei 2022
+|
+Oleh Habib Aditiya, Mahasiswa Universitas Ahmad Dahlan Yogyakarta
+
+Untukmu yang pernah datang dan mengubah segala rencana,
+yang pernah menghentikan segala gundah gulana.
+Untukmu yang pernah menjadikan hariku berwarna dan membuatku kembali percaya pada renjana,
+maaf atas segala hal kecil yang membuatmu marah atas niat baikku yang selalu kau pandang salah.
+Kita saling berjalan perlahan, melengkapi dan saling berjuang.
+Hingga tanpa kita sadari kisah kita hangus jadi arang dan kita tidak lagi punya jalan pulang.
---
+++ read.js
@@ -0,0 +1,29 @@
+Vue.createApp({
+    data() {
+      return {
+        article: null,
+      };
+    },
+    methods: {
+      getMarkdownData() {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const article = urlParams.get("article");
+        var converter = new showdown.Converter();
+        console.log(article);
+        axios
+          .get("https://raw.githubusercontent.com/Habibaditiya21/tekweb2022/master/contents/" + article)
+          .then((res) => {
+            var html = converter.makeHtml(res.data);
+            this.article = html;
+            console.log(html);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+    },
+    beforeMount() {
+      this.getMarkdownData();
+    },
+  }).mount("#app");
