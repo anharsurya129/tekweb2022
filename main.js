3ab24c66686a5d2634b3c43b245df610822982e1
anharsurya129 <97265641+anharsurya129@users.noreply.github.com>
2022-05-20 21:01:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -73,17 +73,3 @@ Vue.createApp({
     },
 }).mount("#arc");
        
-$('.skill-per').each(function() {
-  var $this = $(this);
-  var per = $this.attr('per');
-  $this.css("width",per+'%');
-  $({animatedValue: 0}).animate({animatedValue: per}, {
-    duration: 1000,
-    step: function(){
-      $this.attr('per', Math.floor(this.animatedValue) + '%');
-    },
-    complete: function(){
-      $this.attr('per', Math.floor(this.animatedValue) + '%');
-    }
-  });
-});
